1b17ec94085de891b319d9281f86a531deed4147
calispac <cyril.alispach@unige.ch>
2017-09-01 16:56:53 +0200

    package fixed

--- sandbox/gammapy_test.py
+++
@@ -1,3 +0,0 @@
-from gammapy.catalog import source_catalogs
-gammacat = source_catalogs.info()
-print(gammacat)
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name='digicam_scheduling',
+    name='digicamscheduling',
     version='0.1.0',
-    packages=['inout', 'core', 'display', ''],
+    packages=['inout', 'core', 'display',],
     url='https://github.com/calispac/digicam_scheduling',
     license='GNU GPL 3.0',
     author='Cyril Alispach',
     author_email='cyril.alispach@gmail.com',
     scripts=['schedule.py', 'visibility.py', 'elevation.py', 'crab_visibility.py'],
     description='A package for observation scheduling in gamma-ray astronomy',
-    requires=['numpy', 'astropy', 'PyAstronomy', 'matplotlib', 'scipy']
+    requires=['numpy', 'astropy', 'PyAstronomy', 'matplotlib', 'scipy'],
 )
