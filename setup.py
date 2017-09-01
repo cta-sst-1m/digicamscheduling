6053cd89fdc050bfd4c09546732c6a2e48cff6f5
calispac <cyril.alispach@unige.ch>
2017-09-01 17:01:49 +0200

    package fixed

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='digicamscheduling',
+    name='digicam_scheduling',
     version='0.1.0',
-    packages=['inout', 'core', 'display',],
+    packages=['digicam_scheduling',],
     url='https://github.com/calispac/digicam_scheduling',
     license='GNU GPL 3.0',
     author='Cyril Alispach',
     author_email='cyril.alispach@gmail.com',
-    scripts=['schedule.py', 'visibility.py', 'elevation.py', 'crab_visibility.py'],
+    scripts=['bin/schedule.py', 'bin/visibility.py', 'bin/elevation.py', 'bin/crab_visibility.py'],
     description='A package for observation scheduling in gamma-ray astronomy',
     requires=['numpy', 'astropy', 'PyAstronomy', 'matplotlib', 'scipy'],
 )
