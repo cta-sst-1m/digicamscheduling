92c8f28ad464fc0ac647f42130f6dba2f33ae5df
calispac <cyril.alispach@unige.ch>
2017-09-02 12:43:59 +0200

    setup.py working

---
+++ build/lib/digicamscheduling/core/gamma_source.py
@@ -0,0 +1,31 @@
+import astropy.units as u
+from astropy.coordinates import SkyCoord, AltAz
+import numpy as np
+
+
+def compute_source_position(date, location, ra, dec):
+
+    altaz_coordinates = AltAz(obstime=date, location=location)
+    source_location = SkyCoord(ra=ra, dec=dec, frame='icrs')
+    source_altaz = source_location.transform_to(altaz_coordinates)
+
+    return source_altaz
+
+
+def compute_source_intensity(alt, trees=True):
+
+    if trees:
+
+        return np.sin(alt) * (alt > 45 * u.deg) * (alt < 85 * u.deg)
+
+    else:
+
+        return np.sin(alt) * (alt > 0 * u.deg)
+
+
+def intensity(date, location, ra, dec):
+
+    alt = compute_source_position(date, location, ra, dec).alt
+
+    return compute_source_intensity(alt)
+
